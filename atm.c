1916eb5c9526c641fdc8916410ae04595c090b59
VictorManzini <vikmanzini@icloud.com>
2026-05-19 20:49:07 -0300

    Testado o codigo atm.c

--- atm.c
+++ atm.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 void exibirMenu();
 void consultarSaldo(float saldo);
 float realizarSaque(float saldo);
@@ -15,7 +16,8 @@ int main() {
     float saldo = 500.0;
     int opcao; 
     do {
-        system("cls");
+        system("clear"); //Para Mac e Linux
+        //system("cls");  //para Windows
         exibirMenu();
         scanf("%d", &opcao);
         if(opcao <0 || opcao > 4){
@@ -25,27 +27,27 @@ int main() {
 
         switch(opcao){
             case 1: 
-            printf("Opcao 1, Consultar saldo, selecionada\n");
+            printf("\nOpcao 1, Consultar saldo, selecionada\n");
             consultarSaldo(saldo);
             break;
 
             case 2: 
-            printf("Opcao 2, Realizar saque, selecionada\n");
+            printf("\nOpcao 2, Realizar saque, selecionada\n");
             saldo = realizarSaque(saldo);
             break;
 
             case 3:
-            printf("Opcao 3, Realizar deposito, selecionada\n"); 
+            printf("\nOpcao 3, Realizar deposito, selecionada\n"); 
             saldo = realizarDeposito(saldo);
             break;
 
             case 4: 
-            printf("Opcao 4, Ver o extrato, selecionada\n");
+            printf("\nOpcao 4, Ver o extrato, selecionada\n");
             exibirExtrato(saldo); 
             break;
 
             case 0: 
-            printf("Opcao Sair selecionada...\n");
+            printf("\nOpcao Sair selecionada...\n");
             printf("Obrigado por usar nossa ATM\n");
             printf("Ate logo...\n");
             break;
@@ -64,7 +66,7 @@ void exibirMenu() {
     printf("\n========ATM=======\n");
     printf("Selecione as opcoes abaixo: \n");
     printf("1 - Consultar saldo\n");
-    printf("2 -  Realizar saque\n");
+    printf("2 - Realizar saque\n");
     printf("3 - Realizar deposito\n");
     printf("4 - Ver o extrato\n");
     printf("0 - Encerrar o programa\n"); 
@@ -75,7 +77,7 @@ void exibirMenu() {
 
 void consultarSaldo(float saldo) {
     printf("Saldo disponivel %.2f\n", saldo);
-
+    printf("\n");
     printf("Precione Enter para continuar...");
     getchar();
     printf("\n");
@@ -88,7 +90,7 @@ float realizarSaque(float saldo) {
     int sucesso = 0;
     
     do {
-        printf("Digite o valor que deseja sacar: ");
+        printf("\nDigite o valor que deseja sacar: ");
         scanf("%f", &valor);
         while(getchar() != '\n');
         
@@ -96,7 +98,7 @@ float realizarSaque(float saldo) {
             printf("Valor invalido... tente novamente\n");
         }
         else if(valor > saldo) {
-            printf("Saldo insuficiente... tente novamente\n");
+            printf("\nSaldo insuficiente... tente novamente\n");
         }
         else {
             printf("Saque de R$ %.2f realizado\n", valor);
@@ -105,30 +107,34 @@ float realizarSaque(float saldo) {
             sucesso = 1;
         }
     } while(sucesso == 0);
+    printf("\nPrecione Enter para continuar...\n");
+    getchar();
 
     return saldo;
 }
 
 float realizarDeposito(float saldo) {
     float valor;
+    int sucesso = 0;
 
-    do{
-        printf("Digite o valor que deseja depositar: ");
+    do {
+        printf("\nDigite o valor que deseja depositar: ");
         scanf("%f", &valor);
         while(getchar() != '\n');
-        if(valor <= 0){
-            printf("Valor invalido, tente novamente...\n"); 
-        }
-        else{
-            printf("Valor depositado: %.2fR$\n", valor);
-            saldo += valor; 
+        if(valor <= 0) {
+            printf("\nValor invalido, tente novamente...\n");
+        } else {
+            printf("\nValor depositado: R$ %.2f\n", valor);
+            saldo += valor;
             registrarOperacao('D', valor);
+            sucesso = 1;
         }
-    }while(valor <= 0);
+    } while(sucesso == 0);
+    printf("Precione Enter para continuar...\n");
+    getchar();
 
     return saldo;
 }
-
 
 void registrarOperacao(char tipo, float valor) {
     tipoOp[0]  = tipoOp[1];
@@ -154,10 +160,10 @@ void exibirExtrato(float saldo) {
             printf("Deposito realizado; valor: %.2fR$\n", valorOp[i]);
         }
         else{
-            printf("ERRO!");
+            printf("\nERRO!\n");
         }
         }
-    printf("Saldo atual %.2fR$\n", saldo); 
+    printf("Saldo atual: %.2fR$\n", saldo); 
     printf("=============================\n");
     printf("Pressione Enter para continuar:\n");
     getchar();
